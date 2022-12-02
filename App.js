d1e5ac415ae40cd75e3bd668f99eafb52a8f13c2
sihyun <si4018@naver.com>
2022-12-02 22:36:25 +0900

    first

--- App.js
+++ App.js
@@ -1,20 +1,93 @@
+import {Location} from "expo-location";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
+const { width:SCREEN_WIDTH} = Dimensions.get("window");
 
 export default function App() {
+  const [city, setCity] = useState("Loading...");
+  const [location, setLocation] = useState();
+  const [ok, setOk] = useState(true);
+  const ask = async () => {
+    const {granted} = await Location.requestPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+      }
+      const {
+        coords: {latitude, longitude },
+      } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+      const location = await Location.reverseGeocodeAsync(
+        { latitude, longitude },
+        { useGoogleMaps:  false }
+      );
+      console.log(location)
+      setCity(location[0].city);
+    };
+    useEffect(() => {
+      ask();
+    },[]);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>{city}</Text>
+      </View>
+      <ScrollView 
+        pagingEnabled
+        horizontal
+        showsVerticalScrollIndicator={false}
+        contentContainerstyle={styles.weather}
+        >
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.descripstion}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.descripstion}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.descripstion}>Sunny</Text>
+        </View>        
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.descripstion}>Sunny</Text>
+        </View>
+      </ScrollView> 
     </View>
   );
 }
 
+// flex direcxtion의 기본값은 row인데 모바일에서는 column이다.
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    flex : 1,
+    backgroundColor: "tomato",
+  },
+  city:{
+    flex: 0.8,
+    justifyContent: "center",
+    alignItems : "center",
+  },
+  cityName:{
+    fontSize: 60,
+    fontWeight : "550",
+  },
+  weather : {
+    flex: 3,
+  },
+  day:{
+    width: SCREEN_WIDTH,
+    alignItems:"center",
+    backgroundColor: "teal",
+  },
+  temp:{
+    marginTop: 20,
+    fontSize: 158,
+  },
+  descripstion:{
+    marginTop: -30,
+    fontSize:60,
   },
 });
