729845ee677d4560508bc0f500f35a0b6c77c79c
sihyunjojo <87458171+sihyunjojo@users.noreply.github.com>
2022-12-05 17:26:37 +0900

    위치api 연동 (완)

--- App.js
+++ App.js
@@ -1,42 +1,38 @@
-import {Location} from "expo-location";
+import * as Location from 'expo-location';
 import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
-const { width:SCREEN_WIDTH} = Dimensions.get("window");
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
-  const [city, setCity] = useState("Loading...");
+  const [city,setCity] = useState("Loading...");
   const [location, setLocation] = useState();
   const [ok, setOk] = useState(true);
   const ask = async () => {
-    const {granted} = await Location.requestPermissionsAsync();
+    const { granted } = await Location.requestForegroundPermissionsAsync();
     if (!granted) {
-      setOk(false);
-      }
-      const {
-        coords: {latitude, longitude },
-      } = await Location.getCurrentPositionAsync({ accuracy: 5 });
-      const location = await Location.reverseGeocodeAsync(
-        { latitude, longitude },
-        { useGoogleMaps:  false }
-      );
-      console.log(location)
-      setCity(location[0].city);
-    };
-    useEffect(() => {
-      ask();
-    },[]);
+      setOk(false)
+    }
+    const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false })
+    setCity(location[0].city);
+  };
+  useEffect(() => {
+    ask();
+  }, []);
   return (
     <View style={styles.container}>
       <View style={styles.city}>
         <Text style={styles.cityName}>{city}</Text>
       </View>
-      <ScrollView 
+      <ScrollView
         pagingEnabled
         horizontal
         showsVerticalScrollIndicator={false}
         contentContainerstyle={styles.weather}
-        >
+      >
         <View style={styles.day}>
           <Text style={styles.temp}>27</Text>
           <Text style={styles.descripstion}>Sunny</Text>
@@ -48,12 +44,12 @@ export default function App() {
         <View style={styles.day}>
           <Text style={styles.temp}>27</Text>
           <Text style={styles.descripstion}>Sunny</Text>
-        </View>        
+        </View>
         <View style={styles.day}>
           <Text style={styles.temp}>27</Text>
           <Text style={styles.descripstion}>Sunny</Text>
         </View>
-      </ScrollView> 
+      </ScrollView>
     </View>
   );
 }
@@ -61,33 +57,33 @@ export default function App() {
 // flex direcxtion의 기본값은 row인데 모바일에서는 column이다.
 
 const styles = StyleSheet.create({
-  container:{
-    flex : 1,
+  container: {
+    flex: 1,
     backgroundColor: "tomato",
   },
-  city:{
+  city: {
     flex: 0.8,
     justifyContent: "center",
-    alignItems : "center",
+    alignItems: "center",
   },
-  cityName:{
+  cityName: {
     fontSize: 60,
-    fontWeight : "550",
+    fontWeight: "550",
   },
-  weather : {
+  weather: {
     flex: 3,
   },
-  day:{
+  day: {
     width: SCREEN_WIDTH,
-    alignItems:"center",
+    alignItems: "center",
     backgroundColor: "teal",
   },
-  temp:{
+  temp: {
     marginTop: 20,
     fontSize: 158,
   },
-  descripstion:{
+  descripstion: {
     marginTop: -30,
-    fontSize:60,
+    fontSize: 60,
   },
 });
